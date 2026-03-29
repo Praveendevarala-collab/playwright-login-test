2248d98a4e1f22b9aa9d5cd98a53c9134d4f2969
Praveen Devarala <your-email@gmail.com>
2026-03-29 19:44:43 +0530

    demologin page

---
+++ playwright.config.js
@@ -0,0 +1,7 @@
+const { devices } = require('@playwright/test');
+
+projects: [
+  { name: 'chromium', use: { ...devices['Desktop Chrome'] } },
+ // { name: 'firefox', use: { ...devices['Desktop Firefox'] } },
+ // { name: 'webkit', use: { ...devices['Desktop Safari'] } },
+]
--- tests/example.spec.js
+++
@@ -1,7 +0,0 @@
-// @ts-check
-const { test, expect } = require('@playwright/test');
-
-test('example test', async ({ page }) => {
-  await page.goto('https://example.com');
-  await expect(page).toHaveTitle(/Example/);
-});
