9d49cadc89f9126287d9fef4cc2ec57aa0eb5b91
Praveen Devarala <your-email@gmail.com>
2026-03-30 21:14:08 +0530

    Added login validation in auth setup

--- tests/auth.setup.spec.js
+++ tests/auth.setup.spec.js
@@ -3,9 +3,15 @@ const { test } = require('@playwright/test');
 test('login and save session', async ({ page }) => {
   await page.goto('https://demohri.sutihr.com');
 
+  await page.waitForSelector('input[name="username"]');
+
   await page.fill('input[name="username"]', process.env.USERNAME);
   await page.fill('input[type="password"]', process.env.PASSWORD);
+
   await page.click('button:has-text("Proceed")');
+
+  // ✅ Ensure login success
+  await page.waitForURL(/dashboard/);
 
   await page.context().storageState({
     path: 'playwright/.auth/user.json'
